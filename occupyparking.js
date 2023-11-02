f068f770eef3e74642588fd48dcd1c77b0730bac
rae-2 <cloudandelion@gmail.com>
2023-11-02 13:11:06 -0400

    ugh

--- occupyparking.js
+++ occupyparking.js
@@ -1,28 +1,14 @@
+p = '5'
 y = 'C...C'
 T = 'CC..C'
-y[0, 1, 2, 3, 4, 5];
-T[0, 1, 2, 3, 4, 5];
 
-const parking =[
-    1,2,3,4,5,6,7,8,9,10,
-    11,12,13,14,15,16,17,
-];
-
-function what(number,array){
-    let uh;
-    for(let n = 1; n <= array.length; n++){
-        if(array[n] == number){
-        found = array[n];
-            break;
-        } else{
-            console.log(".");
-            }
-}
-        if(!found){
-            console.log("can't be found");
+//put thr functions that poop out the outputs
+function thing(p, y, T) {
+    let match = 0;
+    for (let n = 0; n < p; n++){
+        if(y[n]=== "C" && T[n]==="C"){
+            match++
         }
-        console.log(found);
-        return found;
-
+    }
+    console.log(match);
 }
-what(3, parking);
