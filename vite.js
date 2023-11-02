3c4ac083796fd6ebeb236c10a8d86ccf9e0dcb5f
rae-2 <cloudandelion@gmail.com>
2023-11-02 12:32:58 -0400

    huh

---
+++ vite.js
@@ -0,0 +1,22 @@
+const numsArr = [
+    1,2,3,4,5,6,7,8,9,10,
+];
+
+function linearSearch(num, arr){
+    let found;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === num){
+            found = arr[i];
+            break;
+        } else{
+            console.log("still looking");
+            }
+        }
+        if(!found){
+            console.log("can't be found");
+        }
+        console.log(found);
+        return found;
+}
+linearSearch(3, numsArr);
+
