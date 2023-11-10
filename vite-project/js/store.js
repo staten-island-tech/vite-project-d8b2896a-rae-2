0967156bb91f0a22185bff227ea7b6800953c1ad
rae-2 <cloudandelion@gmail.com>
2023-11-10 12:54:54 -0500

    sssssss

---
+++ vite-project/js/store.js
@@ -0,0 +1,4 @@
+const store = ["poodle", "afghan hound", "alaskan klee kai", "akita", "tosa"];
+const name = ["SCAM"];
+
+export {store, name}
---
+++ vite-project/js/vite.js
@@ -0,0 +1,3 @@
+import "../css/style.css";
+import{store, name} from"./store";
+console.log(store[0] + " is an ugly dog");
